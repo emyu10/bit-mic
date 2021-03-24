0208c79f35efdebd1ee93e963473e8d464a36e7e
Mohamed Usman <emyu10@gmail.com>
2021-03-24 22:06:55 +0500

    java project: some more changes;

--- Java Project/saam_pos/src/pos/saam/ApplicationInterface.java
+++ Java Project/saam_pos/src/pos/saam/ApplicationInterface.java
@@ -1,4 +1,12 @@
 package pos.saam;
 
+import pos.saam.user.User;
+
+import javax.swing.*;
+import java.awt.*;
+
 public interface ApplicationInterface {
+    Container getContentPane();
+    JPanel  getContentArea();
+    boolean onLogin(User user);
 }
