38ab0542ce56d8b1eef9011352a795e2336216f4
Mohamed Usman <emyu10@gmail.com>
2021-03-24 22:06:55 +0500

    java project: some more changes;

--- Java Project/saam_pos/src/pos/saam/user/UserModel.java
+++ Java Project/saam_pos/src/pos/saam/user/UserModel.java
@@ -2,6 +2,9 @@ package pos.saam.user;
 
 import pos.saam.utils.Db;
 
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -32,5 +35,35 @@ public class UserModel extends Db {
             e.printStackTrace();
         }
         return this.users;
+    }
+
+    public User getByCredentials(String username, String password) {
+        User user = new User();
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(password.getBytes());
+            byte[] hashPass = md.digest();
+            this.getConnection();
+            String sql = "select id, username, fullname, password, active, admin, addedon from user where username=? and password=?";
+            PreparedStatement stmt = this.conn.prepareStatement(sql);
+            stmt.setString(1, username);
+            stmt.setString(2, DatatypeConverter.printHexBinary(hashPass).toLowerCase());
+            userResult = stmt.executeQuery();
+            if (userResult.next()) {
+                user = new User(
+                        userResult.getInt("id"),
+                        userResult.getString("username"),
+                        userResult.getString("fullname"),
+                        userResult.getString("password"),
+                        userResult.getInt("active") == 1,
+                        userResult.getInt("admin") == 1,
+                        userResult.getString("addedon")
+                );
+            }
+        } catch(NoSuchAlgorithmException | SQLException e) {
+            return user;
+        }
+        return user;
     }
 }
