218640a92131f6b67eda13232636150bf4d945ea
Mohamed Usman <emyu10@gmail.com>
2021-05-22 21:55:39 +0500

    advanced C#: classwork

---
+++ Advanced Windows Forms Programming Using C#/Classwork/DataGridExample/DataGridExample/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DataGridExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = null;
+            SqlConnection connection;
+            SqlCommand command;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+            DataView dv;
+            string sql = null;
+
+            connectionString = "Data Source=192.168.122.17; Initial Catalog = Northwind; User id = sa; Password = mql@9933343";
+
+            sql = "select * from products";
+
+            connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                command = new SqlCommand(sql, connection);
+                adapter.SelectCommand = command;
+                adapter.Fill(ds, "Create DataView");
+                adapter.Dispose();
+                connection.Close();
+
+                dv = ds.Tables[0].DefaultView;
+
+                dataGridView1.DataSource = dv;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
